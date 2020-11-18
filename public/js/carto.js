dc355a04fab8d4fb9fde7bf5f3b5ba85dae9ffe5
Xavier Monset <xavier.monset@gmail.com>
2020-11-18 22:20:13 +0100

    Des pitis changements

--- public/js/carto.js
+++ public/js/carto.js
@@ -2,16 +2,19 @@
 var lat = 45.1386;
 var lon = 1.338196;
 var dateDernierePosition;
+var dateEnregistrement;
 var macarte = null;
 var idInt = 0;
 var idString = "";
 var metronome = setInterval(actualisePosition, 3000);
 var metronomeParcour = setInterval(actualiseDernierParcour, 5000);
 var marker;
-
+var laDivInfo = document.getElementsByClassName("heurPos")[0];
+var laDivEnr = document.getElementsByClassName("heurEnr")[0];
+var carte = document.getElementById("laCarte");
 
 function changeLaTailleDeLaCarte() {
-    let carte = document.getElementById("laCarte");
+
     let hauteurCarte = Math.round(window.innerHeight * .7);
     carte.style.height = hauteurCarte  + "px";
 
@@ -30,6 +33,7 @@ function initMap() {
     }).addTo(macarte);
     marker = L.marker([lat, lon]).addTo(macarte);
     marker.bindTooltip("Je cherche la dernière position").openTooltip();
+    laDivInfo.innerText = "En attente de la réponse du serveur"
 
 }
 function actualisePosition() {
@@ -42,15 +46,28 @@ function actualisePosition() {
     xmlhttp.onreadystatechange = function () {
 
         if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+
             let maReponse = JSON.parse(xmlhttp.responseText);
             let laPosition = maReponse["hydra:member"][0];
+
+
             lat = laPosition["latitude"];
             lon = laPosition["longitude"];
+
             dateDernierePosition = laPosition["datePosition"];
+            dateEnregistrement = laPosition["dateInsertion"];
+            let heureOnly = dateDernierePosition.slice(dateDernierePosition.indexOf("T") +1
+            , dateDernierePosition.indexOf("+"));
+
+            let HeureEnregistrement = dateEnregistrement.slice(dateEnregistrement.indexOf("T") +1
+                , dateEnregistrement.indexOf("+"));
+
             macarte.setView([lat, lon]);
             marker.setLatLng([lat, lon]);
-            marker.bindTooltip(dateDernierePosition).openTooltip();
-           // marker = L.marker([lat, lon]).addTo(macarte).bindPopup(dateDernierePosition);
+            marker.bindTooltip(heureOnly).openTooltip();
+            laDivInfo.innerText = "Heure de la position : " + heureOnly;
+            laDivEnr.innerText = "Heure réception : " + HeureEnregistrement;
+
         }
     }
     xmlhttp.open("GET", url, true);
@@ -69,6 +86,8 @@ function actualiseDernierParcour() {
                 idInt = 0;
                 idString = "";
                 marker.bindTooltip("Pas de parcour en cours ...").openTooltip();
+                laDivInfo.innerText = "Pas de parcours ... en cours"
+                laDivEnr.innerText = "";
 
             } else {
                 idInt = leParcour["id"];
@@ -79,11 +98,12 @@ function actualiseDernierParcour() {
     xmlhttp.open("GET", "http://api.xav/api/parcours?order%5Bid%5D=desc&page=1&itemsPerPage=1", true);
     xmlhttp.send(null);
 }
-window.onload = function(){
+window.onload = function() {
     // Fonction d'initialisation qui s'exécute lorsque le DOM est chargé
     changeLaTailleDeLaCarte();
     initMap();
     actualiseDernierParcour();
+
 };
 window.onresize = function (){
     changeLaTailleDeLaCarte();
